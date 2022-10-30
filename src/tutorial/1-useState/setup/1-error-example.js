d7798783b723a813e41c3f92b45e25d8c5cb4456
lvuk <luka.vuk456@gmail.com>
2022-10-30 12:50:03 +0100

    2-useState-basics DONE

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title';
+  return (
+    <>
+      <h2>{title}</h2>
+    </>
+  );
 };
 
 export default ErrorExample;

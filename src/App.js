d8db6e0041c9ce33a0c91a94e3495d9affe1039c
lvuk <luka.vuk456@gmail.com>
2022-10-30 13:04:29 +0100

    3-useState-array DONE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/2-useState-basics';
-import Final from './tutorial/1-useState/final/1-error-example';
+import Setup from './tutorial/1-useState/setup/3-useState-array';
 
 function App() {
   return (

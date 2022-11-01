856c9cc4990c7d87d1567dfcbc3c34d774ab402a
lvuk <luka.vuk456@gmail.com>
2022-11-01 18:05:17 +0100

    2-useEffect-cleanup DONE

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,24 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setSize(window.innerWidth);
+    });
+    // return () => {
+    //   console.log('cleanup');
+    //   window.removeEventListener('resize', setSize(window.innerWidth));
+    // };
+  }, []);
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
